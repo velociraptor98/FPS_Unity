de0561a45b09473aa536487b12a3c53a3dffe056
Kunal Singh <kunal1998doon@gmail.com>
2020-05-05 20:40:02 +0530

    Completed player movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,36 +4,76 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 10.0f;
+    private Animator anim;
+    [SerializeField] private float playerSpeed = 8.0f;
+    [SerializeField] private float gravityModifier = 2.0f;
+    [SerializeField] private float jumpHeight = 2.0f;
     private CharacterController character;
     private Vector3 moveInput;
     [SerializeField]private Transform camera;
-    [SerializeField] private float mouseSensitivity = 1;
+    [SerializeField] private float mouseSensitivity = 2;
     [SerializeField] private bool isMouseInverted = false;
+    private bool canJump;
+    [SerializeField]
+    private Transform groundCheckPoint;
+    [SerializeField]
+    private float runSpeed = 15.0f;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    private bool canDoubleJump;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = this.GetComponent<Animator>();
         character = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //moveInput.x = Input.GetAxis("Horizontal")*playerSpeed*Time.deltaTime;
-        //moveInput.z = Input.GetAxis("Vertical")*playerSpeed*Time.deltaTime;
+        float yStore = moveInput.y;
         Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
         Vector3 horMove = transform.right * Input.GetAxis("Horizontal") ;
         moveInput = vertMove + horMove;
         moveInput.Normalize();
-        moveInput *= playerSpeed;
-        if(character)
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveInput *= runSpeed;
+        }
+        else
+        {
+            moveInput *= playerSpeed;
+        }
+        moveInput.y = yStore;
+        moveInput.y += Physics.gravity.y * gravityModifier* Time.deltaTime;
+        if(character.isGrounded)
+        {
+            moveInput.y = Physics.gravity.y * gravityModifier*Time.deltaTime;
+        }
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, 0.25f,whatIsGround).Length>0 ; 
+        if(canJump)
+        {
+            canDoubleJump = false;
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            moveInput.y = jumpHeight;
+            canDoubleJump = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && canDoubleJump)
+        {
+            moveInput.y = jumpHeight;
+            canDoubleJump = false;
+        }
+        if (character)
         {
             character.Move(moveInput * Time.deltaTime);
         }
-        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y+mouseInput.x,transform.rotation.eulerAngles.z);
         camera.rotation = Quaternion.Euler(camera.rotation.eulerAngles + new Vector3(-mouseInput.y, 0.0f, 0.0f));
-        
+        anim.SetFloat("MoveSpeed",moveInput.magnitude);
+        anim.SetBool("OnGround", canJump);
     }
 }
